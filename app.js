c5d173b84c115d8c540810d98bcda8eb2cfbc782
bunnyBites <s.mukeshkr481@gmail.com>
2023-03-09 15:15:09 +0530

    added dynamic weather forecast for provided city name

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const express = require("express");
-const https = require("https");
-const app = express();
+  const express = require("express");
+  const https = require("https");
+  const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  const weatherPathUrl = "https://api.openweathermap.org/data/2.5/weather?lat=10.804973&lon=78.6870296&appid=5a240c73d4ccc6381da9f0114cc006e0";
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-  https.get(weatherPathUrl, (response) => {
-    console.log(response);
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  })
 
-    response.on("data", (data) => {
-      // parse the fetched data
-      const weatherObj = JSON.parse(data);
+  app.post("/", (req, res) => {
 
-      // weather description
-      const weatherDescription = weatherObj.weather[0].description;
+    const cityName = req.body.cityName;
+    const apiKey = "5a240c73d4ccc6381da9f0114cc006e0";
+    const weatherPathUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-      // weather temperature
-      const weatherTemperature = weatherObj.main.temp;
-      const imgPath = `https://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png`;
+    https.get(weatherPathUrl, (response) => {
+      response.on("data", (data) => {
+        // parse the fetched data
+        const weatherObj = JSON.parse(data);
 
-      res.write("<p>Your current weather is: " + weatherDescription + " </p>");
-      res.write("<h2>Temperature in your location is " + weatherTemperature +" </h2>");
-      res.write("<img src=" + imgPath +  ">");
-      res.send();
-    })
-  });
-})
+        // weather description
+        const weatherDescription = weatherObj.weather[0].description;
 
-app.listen(3002, () => console.log("Server running at port 3002"));
+        // weather temperature
+        const weatherTemperature = weatherObj.main.temp;
+        const imgPath = `https://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png`;
+
+        res.write("<p>Your current weather is: " + weatherDescription + " </p>");
+        res.write("<h2>Temperature in " + weatherObj.name + " is " + weatherTemperature +" deg C </h2>");
+        res.write("<img src=" + imgPath +  ">");
+        res.send();
+      })
+    });
+  })
+
+  app.listen(3002, () => console.log("Server running at port 3002"));
